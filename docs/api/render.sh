3dd463441cfbef4182f055593aa9fd4d560c10cd
Ayco Holleman <ayco.holleman@naturalis.nl>
2017-09-27 11:55:11 +0200

    ok

--- docs/api/render.sh
+++ docs/api/render.sh
@@ -1,1 +1,1 @@
-java -jar raml-slate.jar -raml staging.raml -ex DatasetObject -ex ResultSet -logo ../../logo/col-logo-trans.png -css gruvbox-dark -out .
+java -jar raml-slate.jar -raml ./staging.raml -ex DatasetObject -ex ResultSet -logo ../../logo/col-logo-trans.png -css gruvbox-dark -out .
