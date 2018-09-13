159ca62c5a86f7c4441395cc3a8411248d184e42
mdoering <mdoering@gbif.org>
2018-09-13 13:07:29 +0200

    Start documenting admin API and place types and traits in include file. Update API resources to scoping dataset and various smallerr fixes

--- docs/api/render.sh
+++ docs/api/render.sh
@@ -1,1 +1,3 @@
 java -jar raml-slate.jar -raml ./api.raml -ex DatasetObject -ex ResultSet -logo ../../logo/col-logo-trans.png -css gruvbox-dark -out .
+
+java -jar raml-slate.jar -raml ./admin.raml -ex DatasetObject -ex ResultSet -logo ../../logo/col-logo-trans.png -css gruvbox-dark -out .
