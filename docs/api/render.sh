79753c534fc003d20ec94491abf019ac5966279a
mdoering <mdoering@gbif.org>
2018-09-18 14:32:40 +0200

    Various updates including using id instead of key

--- docs/api/render.sh
+++ docs/api/render.sh
@@ -1,3 +1,3 @@
-java -jar raml-slate.jar -raml ./api.raml -ex DatasetObject -ex ResultSet -logo ../../logo/col-logo-trans.png -css gruvbox-dark -out .
+java -jar raml-slate.jar -raml ./api.raml -ex DatasetObject -ex VerbatimObject -ex VerbatimIdObject -ex VerbatimKeyObject -ex ResultSet -logo ../../logo/col-logo-trans.png -css gruvbox-dark -out .
 
-java -jar raml-slate.jar -raml ./admin.raml -ex DatasetObject -ex ResultSet -logo ../../logo/col-logo-trans.png -css gruvbox-dark -out .
+java -jar raml-slate.jar -raml ./admin.raml -ex DatasetObject -ex VerbatimObject -ex VerbatimIdObject -ex VerbatimKeyObject -ex ResultSet -logo ../../logo/col-logo-trans.png -css gruvbox-dark -out .
