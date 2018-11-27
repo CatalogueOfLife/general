54868027c3ff94e709929a175591672cd193c55d
mdoering <mdoering@gbif.org>
2018-11-27 16:05:24 +0100

    Update dbschema to latest postgres db

--- docs/api/render.sh
+++ docs/api/render.sh
@@ -1,3 +1,1 @@
 java -jar raml-slate.jar -raml ./api.raml -ex DatasetObject -ex VerbatimObject -ex VerbatimIdObject -ex VerbatimKeyObject -ex ResultSet -logo ../../logo/col-logo-trans.png -css gruvbox-dark -out .
-
-java -jar raml-slate.jar -raml ./admin.raml -ex DatasetObject -ex VerbatimObject -ex VerbatimIdObject -ex VerbatimKeyObject -ex ResultSet -logo ../../logo/col-logo-trans.png -css gruvbox-dark -out .
