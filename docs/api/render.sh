52c41de628defba2a66d09896a3810c2a99993da
Markus Döring <m.doering@mac.com>
2019-11-29 09:18:57 +0100

    Use new logo

--- docs/api/render.sh
+++ docs/api/render.sh
@@ -1,1 +1,1 @@
-java -jar raml-slate.jar -raml ./api.raml -ex DatasetObject -ex VerbatimObject -ex VerbatimIdObject -ex VerbatimKeyObject -ex ResultSet -logo ../../logo/col-logo-trans.png -css gruvbox-dark -out .
+java -jar raml-slate.jar -raml ./api.raml -ex DatasetObject -ex VerbatimObject -ex VerbatimIdObject -ex VerbatimKeyObject -ex ResultSet -logo ../../logo/col-logo.jpg -title "CoL API" -css gruvbox-dark -out .
